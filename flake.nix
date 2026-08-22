5674d27b9c36050afa0050b1d5ba1bbe9eb4106c
Nikita Pedorich <pedorich.n@gmail.com>
2026-08-22 23:32:11 +0900

    Add Cachix flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "My personal NUR repository";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://pedorich-n-nur.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "pedorich-n-nur.cachix.org-1:EisUgiRsKFmZ3LJN7r29oDae+Wxq9FQpkcydRx19N7Q="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
