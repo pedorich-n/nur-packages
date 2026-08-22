09ecf1fe381f2cb029e1d029ccb9b65116f981d3
Nikita Pedorich <pedorich.n@gmail.com>
2026-08-22 21:41:57 +0900

    Add flake-parts

--- flake.nix
+++ flake.nix
@@ -1,18 +1,37 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in
-    {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
-      nixosModules = import ./nixos-modules;
-      # homeModules = import ./home-modules;
-      # darwinModules = import ./darwin-modules;
-      # flakeModules = import ./flake-modules;
-    };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = lib.systems.flakeExposed;
+
+        perSystem =
+          {
+            config,
+            pkgs,
+            lib,
+            ...
+          }:
+          {
+            legacyPackages = import ./default.nix { inherit pkgs; };
+
+            packages = lib.filterAttrs (_: v: lib.isDerivation v) config.legacyPackages;
+          };
+
+        flake = {
+          nixosModules = import ./nixos-modules;
+          # homeModules = import ./home-modules;
+          # darwinModules = import ./darwin-modules;
+          # flakeModules = import ./flake-modules;
+        };
+      }
+    );
 }
